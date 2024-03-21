d78482833505be49bfee8fef9ef42b86c6afd498
Ahmet Karamercan <ahmet@Ahmet-MacBook-Air-2.local>
2024-03-22 00:01:44 +0300

    login logaout yapıldı

---
+++ bitirme/models.py
@@ -0,0 +1,30 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+from sqlalchemy import CheckConstraint
+from datetime import datetime
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+
+    # One User can have many Chats
+    chats = db.relationship('Chat', backref='author', lazy=True)
+    def __repr__(self):
+        return f"User('{self.first_name}', '{self.email}')"
+
+
+class Chat(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    baslik = db.Column(db.String(200))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    kisa_aciklama = db.Column(db.String(100), nullable = True)
+    #chat_history = db.relationship('...')
+
+    def __repr__(self):
+        return f"Chat('{self.title}', '{self.date_posted}')"
---
+++ bitirme/views.py
@@ -0,0 +1,76 @@
+from flask import Blueprint, Flask, render_template, request, jsonify, flash, redirect
+import time
+from flask_login import login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User, Chat
+import os
+from . import db
+import secrets
+#from PIL import Image #fotoğrafları gösterirken lazım
+#import openai
+
+views = Blueprint('views', __name__)
+
+@views.route("/about")
+@login_required
+def about():
+    return render_template("about.html", user=current_user)
+
+
+@views.route("/")
+@login_required
+def home():
+    message_text = None
+    """if request.method == 'POST':  # chatlar arası dolaşmanın yapılacağı zaman açılcak
+        message_text = request.form.get('message')
+        print(message_text)
+        
+        #gpt den cevap"""
+    
+
+    # SQL sorgusu
+    """chat_history = db.engine.execute("SELECT Chat.id,baslik,date,kisa_aciklama, (chat_history (tanımlamadık))"+
+            " FROM Chat, User" +
+            " WHERE chat.user_id = User.id """ """# AND chat.id IN (== demek)" +
+            " ORDER BY COUNT(*)" +
+            " DESC LIMIT 5)")
+            """
+
+  
+    
+    chat_history = [[0, "selam nasılsın ? Nasıl yardımcı olabilirim ?"], [1,"Saçma sapan cümleler kur"], [0, "cevap 1 asdasdadasdadsadasdasasdasdasdasdasdas asd asd asd as dasdasdasda sdas dasd asdadasdasdasd"], [1, "Teşekkür ederim"]]
+    if message_text != None:
+        chat_history.append([0, message_text + "  senden naber"])
+    other_chats = [["Hava durumu", "bugünün verilerine göre hava durumu ..."],["chat 2", "asdasd asd.as.das as..."],["chat 3", "cqwdasd asda sdasd asd..."]]
+
+    return render_template("index.html", user=current_user, chats = chat_history, other_chats = other_chats)
+
+
+@views.route("/send_message", methods=['POST'])
+@login_required
+def send_message():
+    message_text = request.form['message']
+    print(message_text)
+    time.sleep(5)
+    # GPT ye sorgu atma kısmını burada yap
+    """
+  
+    openai.api_key = "satın alcaz siteden oluşturcaz"
+
+    response = openai.Completion.create(
+        engine="gpt-3.5",
+        prompt="Merhaba, benim adım ChatGPT. Sana nasıl yardımcı olabilirim?",
+        max_tokens=50,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    message = response.choices[0].text
+    print(message)
+    """
+
+
+    response_message = "Bu bir örnek cevaptır."
+    return jsonify({"message": response_message})
+    
